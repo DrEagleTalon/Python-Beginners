c517b3c324f4edcd347e36714bc18eef7cbcffc6
Talan Saunders <Saundersfinancial@outlook.com>
2021-03-06 15:20:04 -0500

    a few new lines to better hone my skillz

--- 3-comments.py
+++ 3-comments.py
@@ -1,16 +1,18 @@
 # program to take the users input
 city = 'Peru' 
 state = 'indiana' 
+zipcode = '46970'
+
+# user input their address
 address = input("enter your street address: ")
 name = input('enter your name: ') # take input
+
+# show user their name and address
 print('your name is', name) # print the name variable 
-print('your address is', address, city, state)
+print('your address is', address, city, state, zipcode)
 
 # answer = input('is this correct?')
 # if('answer = yes' )
 # common practice to comment out code to debug
 
-# using the cmd + / will start a comment or make a line of hughlighted code a comment 
-
-
-
+# using the cmd + / will start a comment or make a line of hughlighted code a comment
